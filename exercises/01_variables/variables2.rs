fb497e45cd7c4d288e7c519198ad9f0e30b835b9
Blimmy <blimgye@gmail.com>
2025-12-08 03:11:57 +0700

    edit error in var2

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -1,8 +1,8 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    
+
     //
-    let x:i32 ;
+    let x:i32 = 5;
 
     if x == 10 {
         println!("x is ten!");
@@ -13,5 +13,5 @@ fn main() {
 
 
 /*
-   we hv to write the type bc rust cant guess the type 
+   we hv to write the type bc rust cant guess the type
  */
